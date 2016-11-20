08dd71142506a6033f11924bf4f6759cd4ebdf8f
shuba_ivan <shuba.ivan@yandex.ru>
2016-11-20 17:39:57 +0200

    user change

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,26 +3,23 @@ class UsersController < ApplicationController
     @user = User.new
   end
 
+  def show
+    @user = User.find(params[:id])
+  end
+
   def create
     @user = User.new(user_params)
     if @user.save
-      user = User.authenticate(params[:user][:email], params[:user][:password])
-      if user
-        session[:user_id] = user.id
-        redirect_to root_url, :notice => "Logged in!"
-      else
-        flash.now.alert = "Invalid email or password"
-        render "new"
-      end
-      # redirect_to root_url, :notice => "Signed up!"
+      log_in @user
+      flash[:success] = "Welcome to the Sample App!"
+      redirect_to @user
     else
       render "new"
     end
   end
 
   def user_params
-    params.require(:user).permit(
-        :email, :password_confirmation, :password, :password_salt, :password_hash
-    )
+    params.require(:user).permit(:name, :email, :password,
+                                 :password_confirmation)
   end
 end
