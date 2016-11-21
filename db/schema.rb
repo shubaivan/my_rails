94082e4888303f2465c0ba1ebfb3b653e2823b37
shuba_ivan <shuba.ivan@yandex.ru>
2016-11-21 16:30:48 +0200

    remember_digest

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161120111224) do
+ActiveRecord::Schema.define(version: 20161121142937) do
 
   create_table "tasks", force: :cascade do |t|
     t.string   "title"
@@ -29,6 +29,7 @@ ActiveRecord::Schema.define(version: 20161120111224) do
     t.datetime "updated_at",      null: false
     t.string   "name"
     t.string   "password_digest"
+    t.string   "remember_digest"
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
